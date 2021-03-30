df832dd684917f62bc2a295fba37cf21d78999ac
dat-adi <dat.adithya@gmail.com>
2021-03-30 18:59:31 +0530

    Updated .config files

---
+++ .config/fish/config.fish
@@ -0,0 +1,18 @@
+set TERM "xterm-256color"
+
+# git
+alias addup='git add -u'
+alias addall='git add .'
+alias branch='git branch'
+alias checkout='git checkout'
+alias clone='git clone'
+alias commit='git commit -m'
+alias fetch='git fetch'
+alias pull='git pull origin'
+alias push='git push origin'
+alias stat='git status'  # 'status' is protected name so using 'stat' instead
+alias tag='git tag'
+alias newtag='git tag -a'
+
+# bat
+alias cat='bat'
