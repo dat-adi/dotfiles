6b1c4e457e5471b794f56562d862a99682fb5912
dat-adi <dat.adithya@gmail.com>
2021-04-20 00:54:56 +0530

    Updated qtile

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -73,20 +73,42 @@ for i, (name, kwargs) in enumerate(group_names, 1):
     keys.append(Key([mod], str(i), lazy.group[name].toscreen()))        # Switch to another group
     keys.append(Key([mod, "shift"], str(i), lazy.window.togroup(name))) # Send current window to another group
 
+layout_theme = {"border_width": 2,
+                "margin": 2,
+                "border_focus": "e1acff",
+                "border_normal": "1D2330"
+                }
 
 layouts = [
-    layout.Columns(border_focus_stack='#d75f5f'),
-    layout.Max(),
-    layout.Stack(num_stacks=2),
     # layout.Bsp(),
-    layout.Matrix(),
-    layout.MonadTall(),
     # layout.MonadWide(),
-    # layout.RatioTile(),
     # layout.Tile(),
-    layout.TreeTab(),
     # layout.VerticalTile(),
     # layout.Zoomy(),
+    layout.Columns(**layout_theme),
+    layout.Max(**layout_theme),
+    layout.Stack(num_stacks=2, **layout_theme),
+    layout.Matrix(**layout_theme),
+    layout.RatioTile(**layout_theme),
+    layout.MonadTall(**layout_theme),
+    layout.TreeTab(
+         fontsize = 10,
+         sections = ["FIRST", "SECOND", "THIRD", "FOURTH"],
+         section_fontsize = 10,
+         border_width = 2,
+         bg_color = "1c1f24",
+         active_bg = "c678dd",
+         active_fg = "000000",
+         inactive_bg = "a9a1e1",
+         inactive_fg = "1c1f24",
+         padding_left = 0,
+         padding_x = 0,
+         padding_y = 5,
+         section_top = 10,
+         section_bottom = 20,
+         level_shift = 8,
+         vspace = 3,
+        ),
 ]
 
 widget_defaults = dict(
