f533136671acd0d25757a005de306a6290649f69
dat-adi <dat.adithya@gmail.com>
2021-05-16 19:53:45 +0530

    updated search engines and disabled dark mode

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -19,7 +19,7 @@ config.load_autoconfig(False)
 #   - never: Always hide the tab bar.
 #   - multiple: Hide the tab bar if only one tab is open.
 #   - switching: Show the tab bar when switching tabs.
-c.tabs.show = 'switching'
+c.tabs.show = "switching"
 
 # Setting default page for when opening new tabs or new windows with
 # commands like :open -t and :open -w .
@@ -43,141 +43,153 @@ c.url.default_page = "file:///home/dat-adi/dotfiles/browser/src/startpage.html"
 # the search engine name to the search term, e.g. `:open google
 # qutebrowser`.
 # Type: Dict
-c.url.searchengines = {'DEFAULT': 'https://duckduckgo.com/?q={}', 'am': 'https://www.amazon.com/s?k={}', 'aw': 'https://wiki.archlinux.org/?search={}', 'goog': 'https://www.google.com/search?q={}', 'hoog': 'https://hoogle.haskell.org/?hoogle={}', 're': 'https://www.reddit.com/r/{}', 'ub': 'https://www.urbandictionary.com/define.php?term={}', 'wiki': 'https://en.wikipedia.org/wiki/{}', 'yt': 'https://www.youtube.com/results?search_query={}'}
+c.url.searchengines = {
+    "DEFAULT": "https://duckduckgo.com/?q={}",
+    "am": "https://www.amazon.in/s?k={}",
+    "aw": "https://wiki.archlinux.org/?search={}",
+    "goog": "https://www.google.com/search?q={}",
+    "re": "https://www.reddit.com/r/{}",
+    "gh": "https://www.github.com/search?q={}",
+    "wiki": "https://en.wikipedia.org/wiki/{}",
+    "yt": "https://www.youtube.com/results?search_query={}",
+}
 
 # Putting up the start page
 c.url.start_pages = ["file:///home/dat-adi/dotfiles/browser/src/startpage.html"]
 
-config.set("colors.webpage.darkmode.enabled", True)
+# Setting Dark Mode on
+# config.set("colors.webpage.darkmode.enabled", True)
 
 # Text color of the completion widget. May be a single color to use for
 # all columns or a list of three colors, one for each column.
 # Type: List of QtColor, or QtColor
-c.colors.completion.fg = ['#9cc4ff', 'white', 'white']
+c.colors.completion.fg = ["#9cc4ff", "white", "white"]
 
 # Background color of the completion widget for odd rows.
 # Type: QssColor
-c.colors.completion.odd.bg = '#1c1f24'
+c.colors.completion.odd.bg = "#1c1f24"
 
 # Background color of the completion widget for even rows.
 # Type: QssColor
-c.colors.completion.even.bg = '#232429'
+c.colors.completion.even.bg = "#232429"
 
 # Foreground color of completion widget category headers.
 # Type: QtColor
-c.colors.completion.category.fg = '#e1acff'
+c.colors.completion.category.fg = "#e1acff"
 
 # Background color of the completion widget category headers.
 # Type: QssColor
-c.colors.completion.category.bg = 'qlineargradient(x1:0, y1:0, x2:0, y2:1, stop:0 #000000, stop:1 #232429)'
+c.colors.completion.category.bg = (
+    "qlineargradient(x1:0, y1:0, x2:0, y2:1, stop:0 #000000, stop:1 #232429)"
+)
 
 # Top border color of the completion widget category headers.
 # Type: QssColor
-c.colors.completion.category.border.top = '#3f4147'
+c.colors.completion.category.border.top = "#3f4147"
 
 # Bottom border color of the completion widget category headers.
 # Type: QssColor
-c.colors.completion.category.border.bottom = '#3f4147'
+c.colors.completion.category.border.bottom = "#3f4147"
 
 # Foreground color of the selected completion item.
 # Type: QtColor
-c.colors.completion.item.selected.fg = '#282c34'
+c.colors.completion.item.selected.fg = "#282c34"
 
 # Background color of the selected completion item.
 # Type: QssColor
-c.colors.completion.item.selected.bg = '#ecbe7b'
+c.colors.completion.item.selected.bg = "#ecbe7b"
 
 # Foreground color of the matched text in the selected completion item.
 # Type: QtColor
-c.colors.completion.item.selected.match.fg = '#c678dd'
+c.colors.completion.item.selected.match.fg = "#c678dd"
 
 # Foreground color of the matched text in the completion.
 # Type: QtColor
-c.colors.completion.match.fg = '#c678dd'
+c.colors.completion.match.fg = "#c678dd"
 
 # Color of the scrollbar handle in the completion view.
 # Type: QssColor
-c.colors.completion.scrollbar.fg = 'white'
+c.colors.completion.scrollbar.fg = "white"
 
 # Background color for the download bar.
 # Type: QssColor
-c.colors.downloads.bar.bg = '#282c34'
+c.colors.downloads.bar.bg = "#282c34"
 
 # Background color for downloads with errors.
 # Type: QtColor
-c.colors.downloads.error.bg = '#ff6c6b'
+c.colors.downloads.error.bg = "#ff6c6b"
 
 # Font color for hints.
 # Type: QssColor
-c.colors.hints.fg = '#282c34'
+c.colors.hints.fg = "#282c34"
 
 # Font color for the matched part of hints.
 # Type: QtColor
-c.colors.hints.match.fg = '#98be65'
+c.colors.hints.match.fg = "#98be65"
 
 # Background color of an info message.
 # Type: QssColor
-c.colors.messages.info.bg = '#282c34'
+c.colors.messages.info.bg = "#282c34"
 
 # Background color of the statusbar.
 # Type: QssColor
-c.colors.statusbar.normal.bg = '#282c34'
+c.colors.statusbar.normal.bg = "#282c34"
 
 # Foreground color of the statusbar in insert mode.
 # Type: QssColor
-c.colors.statusbar.insert.fg = 'white'
+c.colors.statusbar.insert.fg = "white"
 
 # Background color of the statusbar in insert mode.
 # Type: QssColor
-c.colors.statusbar.insert.bg = '#497920'
+c.colors.statusbar.insert.bg = "#497920"
 
 # Background color of the statusbar in passthrough mode.
 # Type: QssColor
-c.colors.statusbar.passthrough.bg = '#34426f'
+c.colors.statusbar.passthrough.bg = "#34426f"
 
 # Background color of the statusbar in command mode.
 # Type: QssColor
-c.colors.statusbar.command.bg = '#282c34'
+c.colors.statusbar.command.bg = "#282c34"
 
 # Foreground color of the URL in the statusbar when there's a warning.
 # Type: QssColor
-c.colors.statusbar.url.warn.fg = 'yellow'
+c.colors.statusbar.url.warn.fg = "yellow"
 
 # Background color of the tab bar.
 # Type: QssColor
-c.colors.tabs.bar.bg = '#1c1f34'
+c.colors.tabs.bar.bg = "#1c1f34"
 
 # Background color of unselected odd tabs.
 # Type: QtColor
-c.colors.tabs.odd.bg = '#282c34'
+c.colors.tabs.odd.bg = "#282c34"
 
 # Background color of unselected even tabs.
 # Type: QtColor
-c.colors.tabs.even.bg = '#282c34'
+c.colors.tabs.even.bg = "#282c34"
 
 # Background color of selected odd tabs.
 # Type: QtColor
-c.colors.tabs.selected.odd.bg = '#282c34'
+c.colors.tabs.selected.odd.bg = "#282c34"
 
 # Background color of selected even tabs.
 # Type: QtColor
-c.colors.tabs.selected.even.bg = '#282c34'
+c.colors.tabs.selected.even.bg = "#282c34"
 
 # Background color of pinned unselected odd tabs.
 # Type: QtColor
-c.colors.tabs.pinned.odd.bg = 'seagreen'
+c.colors.tabs.pinned.odd.bg = "seagreen"
 
 # Background color of pinned unselected even tabs.
 # Type: QtColor
-c.colors.tabs.pinned.even.bg = 'darkseagreen'
+c.colors.tabs.pinned.even.bg = "darkseagreen"
 
 # Background color of pinned selected odd tabs.
 # Type: QtColor
-c.colors.tabs.pinned.selected.odd.bg = '#282c34'
+c.colors.tabs.pinned.selected.odd.bg = "#282c34"
 
 # Background color of pinned selected even tabs.
 # Type: QtColor
-c.colors.tabs.pinned.selected.even.bg = '#282c34'
+c.colors.tabs.pinned.selected.even.bg = "#282c34"
 
 # Default font families to use. Whenever "default_family" is used in a
 # font setting, it's replaced with the fonts listed here. If set to an
@@ -194,7 +206,7 @@ c.fonts.web.family.fixed = '"SauceCodePro Nerd Font"'
 # either a float value with a "pt" suffix, or an integer value with a
 # "px" suffix.
 # Type: String
-c.fonts.default_size = '11pt'
+c.fonts.default_size = "11pt"
 
 # Font used in the completion widget.
 # Type: Font
@@ -206,16 +218,19 @@ c.fonts.debug_console = '11pt "SauceCodePro Nerd Font"'
 
 # Font used for prompts.
 # Type: Font
-c.fonts.prompts = 'default_size sans-serif'
+c.fonts.prompts = "default_size sans-serif"
 
 # Font used in the statusbar.
 # Type: Font
 c.fonts.statusbar = '11pt "SauceCodePro Nerd Font"'
 
 # Bindings for normal mode
-config.bind('M', 'hint links spawn mpv {hint-url}')
-config.bind('Z', 'hint links spawn alacritty -e youtube-dl {hint-url}')
-config.bind('t', 'set-cmd-text -s :open -t')
-config.bind('xb', 'config-cycle statusbar.show always never')
-config.bind('xt', 'config-cycle tabs.show always never')
-config.bind('xx', 'config-cycle statusbar.show always never;; config-cycle tabs.show always never')
+config.bind("M", "hint links spawn mpv {hint-url}")
+config.bind("Z", "hint links spawn alacritty -e youtube-dl {hint-url}")
+config.bind("t", "set-cmd-text -s :open -t")
+config.bind("xb", "config-cycle statusbar.show always never")
+config.bind("xt", "config-cycle tabs.show always never")
+config.bind(
+    "xx",
+    "config-cycle statusbar.show always never;; config-cycle tabs.show always never",
+)
