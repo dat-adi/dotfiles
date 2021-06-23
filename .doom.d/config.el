23db7762c23e2ffdb502ed0f5f385fe1b8b62af2
dat-adi <dat.adithya@gmail.com>
2021-06-24 00:04:12 +0530

    Added mu4e support and a literate config

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -6,8 +6,25 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
+
+(add-to-list 'load-path "/usr/local/share/emacs/site-lisp/mu4e")
+;;(require 'smtpmail)
 (setq user-full-name "Datta Adithya"
-      user-mail-address "dat.adithya@gmail.com")
+      user-mail-address "adithya.19bce7119@vitap.ac.in"
+      mu4e-get-mail-command "mbsync -c ~/.config/mu4e/mbsyncrc -a"
+      mu4e-update-interval  300
+      mu4e-main-buffer-hide-personal-addresses t
+      message-send-mail-function 'smtpmail-send-it
+      starttls-use-gnutls t
+      smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
+      mu4e-sent-folder "/Maildir/adithya-university/Sent"
+      mu4e-drafts-folder "/Maildir/adithya-university/Drafts"
+      mu4e-trash-folder "/Maildir/adithya-university/Trash"
+      mu4e-maildir-shortcuts
+      '(("/Maildir/adithya-university/Inbox"      . ?i)
+        ("/Maildir/adithya-university/Sent Items" . ?s)
+        ("/Maildir/adithya-university/Drafts"     . ?d)
+        ("/Maildir/adithya-university/Trash"      . ?t)))
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
