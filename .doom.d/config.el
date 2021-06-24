38da27caa47c8d7bc2c3a860a918a4c5915c194c
dat-adi <dat.adithya@gmail.com>
2021-06-24 18:57:20 +0530

    Added mbsyncrc, dired + config.org change

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,17 +1,10 @@
-;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
-
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
+'(dired-clean-confirm-killing-deleted-buffers nil) ;; don't ask whether to kill buffers containing deleted files.
 
 (add-to-list 'load-path "/usr/local/share/emacs/site-lisp/mu4e")
 ;;(require 'smtpmail)
 (setq user-full-name "Datta Adithya"
       user-mail-address "adithya.19bce7119@vitap.ac.in"
-      mu4e-get-mail-command "mbsync -c ~/.config/mu4e/mbsyncrc -a"
+     mu4e-get-mail-command "mbsync -c ~/.config/mu4e/mbsyncrc -a"
       mu4e-update-interval  300
       mu4e-main-buffer-hide-personal-addresses t
       message-send-mail-function 'smtpmail-send-it
@@ -26,50 +19,10 @@
         ("/Maildir/adithya-university/Drafts"     . ?d)
         ("/Maildir/adithya-university/Trash"      . ?t)))
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-one)
 
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
 (setq
  org-agenda-files (quote("~/Documents/org/tasks.org"
                          "~/Documents/org/meetings.org"))
  org-deadline-warning-days 7
  org-directory "~/Documents/org/")
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
-
-
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
