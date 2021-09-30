2746b2579bf80722d4ad95370d1700f0fb60797d
dat-adi <dat.adithya@gmail.com>
2021-10-01 00:23:58 +0530

    Casual checkpoint

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -7,7 +7,7 @@
  '(org-agenda-files
    '("~/Documents/org/0 Career/Club/wsoc.org" "~/Documents/org/timetable.org" "~/Documents/org/exams.org" "~/Documents/org/meetings.org" "~/Documents/org/tasks.org"))
  '(package-selected-packages
-   '(zzz-to-char company-dict flycheck-rust org-alert org-pretty-tags org-plus-contrib auctex)))
+   '(prettier-js zzz-to-char company-dict flycheck-rust org-alert org-pretty-tags org-plus-contrib auctex)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
