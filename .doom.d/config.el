f415d088007d351835065ddf2732f88883e47492
dat-adi <dat.adithya@gmail.com>
2022-04-04 21:50:29 +0530

    Fixed DOOM fonts, and updated README

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -4,6 +4,11 @@
 
 (setq doom-theme 'doom-one)
 
+(setq doom-font (font-spec :family "Source Code Pro" :size 13 :weight 'normal)
+      doom-variable-pitch-font (font-spec :family "Source Code Pro") ; inherits `doom-font''s :size
+      doom-unicode-font (font-spec :family "Source Code Pro" :size 12)
+      doom-big-font (font-spec :family "Source Code Pro" :size 19))
+
 (setq org-agenda-files (quote("~/Documents/org/tasks.org"
                               "~/Documents/org/meetings.org"))
       org-deadline-warning-days 7
