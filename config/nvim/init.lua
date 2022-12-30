0a08b541fac14d4bcfd2dcf7d1b6b6a6e26c4eae
dat-adi <dat.adithya@gmail.com>
2022-12-30 17:49:02 +0530

    feat: set up a new neovim configuration setup
    Assisted by @ThePrimeagen, I've set up a new versioning system.

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,29 +1,11 @@
--------------------------------------------
---       _       _                   _ _
---      | |     | |                 | (_)
---    __| | __ _| |_ ______ __ _  __| |_
---   / _` |/ _` | __|______/ _` |/ _` | |
---  | (_| | (_| | |_      | (_| | (_| | |
---   \__,_|\__,_|\__|      \__,_|\__,_|_|
+-- 
+-- github: @dat-adi
+-- website: datadi.murgi.org
 --
--------------------------------------------
---
---        Instagram : dat.adithya
---           GitHub : @dat-adi
---
--------------------------------------------
+
+-- Importing new configuration
+require("v2")
 
 -- Greeting message
 print("Matteyo Onee-sama")
 
--- Importing base configurations
-require("base.plugins")
-require("base.keybinds")
-
--- Importing themes
-require("palenightfall").setup()
-
--- LSP configuration
-require("treesitter")
-require("statusbar")
-require("lsp")
