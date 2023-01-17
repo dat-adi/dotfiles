8874c84fd830087d15d76d20478c2ed7a35f1a7d
dat-adi <dat.adithya@gmail.com>
2023-01-17 19:19:39 +0530

    feat: added in the old config files for continuity
    Might as well have both. Still removed the treesitter and cmp files
    though.

---
+++ config/nvim/lua/v1/base/plugins.lua
@@ -0,0 +1,38 @@
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin', '~/.config/nvim/plugged')
+
+Plug 'ap/vim-css-color'
+Plug 'neovim/nvim-lspconfig'
+
+Plug 'JoosepAlviste/palenightfall.nvim'
+Plug 'ryanoasis/vim-devicons'
+Plug 'akinsho/toggleterm.nvim'
+
+Plug 'junegunn/goyo.vim'
+Plug 'junegunn/limelight.vim'
+Plug 'junegunn/vim-emoji'
+
+Plug 'nvim-lua/plenary.nvim'
+Plug 'nvim-lua/popup.nvim'
+Plug 'nvim-telescope/telescope.nvim'
+Plug 'nvim-telescope/telescope-ui-select.nvim'
+Plug 'glepnir/galaxyline.nvim'
+Plug 'simrat39/rust-tools.nvim'
+
+Plug 'tpope/vim-fugitive'
+Plug 'hrsh7th/nvim-cmp'
+Plug 'hrsh7th/cmp-nvim-lsp'
+Plug 'hrsh7th/cmp-buffer'
+Plug 'hrsh7th/cmp-path'
+Plug 'hrsh7th/cmp-cmdline'
+Plug 'hrsh7th/nvim-cmp'
+Plug 'nvim-treesitter/nvim-treesitter-textobjects'
+
+vim.call('plug#end')
+
+Plug('nvim-treesitter/nvim-treesitter', {
+    ['do'] = function()
+	vim.cmd(':TSUpdate')
+    end
+})
---
+++ config/nvim/lua/v1/init.lua
@@ -0,0 +1,11 @@
+-- Importing base configurations
+require("v1.base.plugins")
+require("v1.base.keybinds")
+
+-- Importing themes
+require("palenightfall").setup()
+
+-- LSP configuration
+-- require("v1.treesitter")
+-- require("v1.statusbar")
+-- require("v1.lsp")
